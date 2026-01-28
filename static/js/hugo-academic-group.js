f4f29e50041d445c4c622d64b44be7f684854353
Seong Joon Oh <coallaoh@gmail.com>
2026-01-28 07:53:29 +0100

    Fix mobile navbar not closing when clicking menu items
    The smooth scroll handler's return false stopped event propagation,
    preventing delegated click handlers from firing. Moved close logic
    inside the scroll handler.

--- static/js/hugo-academic-group.js
+++ static/js/hugo-academic-group.js
@@ -2,6 +2,13 @@
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     var hash = this.hash;
+
+    // Close mobile navbar when clicking anchor links
+    if ($('.navbar-collapse').hasClass('in')) {
+      $('.navbar-collapse').removeClass('in').attr('aria-expanded', 'false');
+      $('.navbar-toggle').attr('aria-expanded', 'false');
+    }
+
     if (this instanceof SVGAElement){
       var target = $(this.href.baseVal);
       target = target.length ? target : $('[name=' + hash.slice(1) +']');
